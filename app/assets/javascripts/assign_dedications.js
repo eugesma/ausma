ec397d7b50f4b9ffe06b152ffede6a1d8ca116e8
BasHud <eugesma@gmail.com>
2019-11-21 12:53:38 -0300

    Change names

--- app/assets/javascripts/assign_dedications.js
+++ app/assets/javascripts/assign_dedications.js
@@ -3,7 +3,6 @@ $(document)
 .on('change', '.quantity', function() {
   var quant = $(this);
   jQuery(function() {
-    console.log("Entró");
     var nested_form = quant.parents(".nested-fields");
     if(quant.val() == '' ){
       quant.val('0');
@@ -12,4 +11,22 @@ $(document)
       nested_form.find('.total-credit').html(quant = quant.val() * $("#dedication_credit").val());
     }
   });
-})
+});
+
+$(document)
+.on('preparation', '.consultation', '.implementation', function() {
+  var event = $(this);
+  jQuery(function() {
+    var nested_form = event.parents(".nested-fields");
+    var preparation = nested_form.find('.preparation');
+    var consultation = nested_form.find('.consultation');
+    var implementation = nested_form.find('.implementation');
+
+    if(quant.val() == '' ){
+      quant.val('0');
+      nested_form.find('.total-credit').html('0');
+    }else{
+      nested_form.find('.total-credit').html(preparation.val() + consultation.val() + implementation.val());
+    }
+  });
+});
