f95824ad7060af2c146b5dbf129f5baa38cd4585
BasHud <eugesma@gmail.com>
2019-12-30 11:03:57 -0300

    Fix migration to heroku

--- db/migrate/20191228021359_add_post_type_ref_to_posts.rb
+++ db/migrate/20191228021359_add_post_type_ref_to_posts.rb
@@ -1,5 +1,5 @@
 class AddPostTypeRefToPosts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :posts, :post_type, foreign_key: true, null: false
+    add_reference :posts, :post_type, foreign_key: true
   end
 end
