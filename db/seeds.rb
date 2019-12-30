94ad7338338be97d6a96786e022f6302af1cfaac
BasHud <eugesma@gmail.com>
2019-12-30 11:17:05 -0300

    Add to seed some record

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,17 @@ eugeUser.save!
 
 Career.create(name: "TUEV", year_duration: 3)
 Career.create(name: "TUF", year_duration: 3)
+
+PostType.create(name: "Libro", credit: 96)
+PostType.create(name: "Capítulo de libro", credit: 48)
+PostType.create(name: "Publicación científica en revista", credit: 48)
+PostType.create(name: "Publicación técnica en revista", credit: 24)
+PostType.create(name: "Publicación en revista de divulgación", credit: 8)
+PostType.create(name: "Trabajo en congreso, jornada, simposio", credit: 24)
+PostType.create(name: "Resumen en congreso, jornada, simposio", credit: 8)
+
+TeacherFormationRole.create(name: "Asistente", plus_percentage: 0)
+TeacherFormationRole.create(name: "Asistente y aprobado", plus_percentage: 50)
+TeacherFormationRole.create(name: "Expositor", plus_percentage: 50)
+TeacherFormationRole.create(name: "Organizador", plus_percentage: 100)
+TeacherFormationRole.create(name: "Evaluador", plus_percentage: 50)
