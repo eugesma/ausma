6b19e72271f52af9f7aa76d2e7c5c9f1a88f6733
BasHud <eugesma@gmail.com>
2020-03-09 10:17:35 -0300

    Show teacher profile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+  def index
+    if user_signed_in?
+    teacher = current_user.teacher
+    @assignatures = teacher.teacher_assignatures
+    @dedications = teacher.teacher_dedications
+
+    @total_credit_assignatures = teacher.total_credit_assignatures
+    end
+  end
+
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,8 +1,22 @@
 class Teacher < ApplicationRecord
   belongs_to :profile, optional: true
   belongs_to :user, optional: true
+
   has_many :teacher_formations
   has_many :formations, through: :teacher_formations
 
+  has_many :teacher_assignatures
+  has_many :assignatures, through: :teacher_assignatures
+
+  has_many :teacher_dedications
+  has_many :dedications, through: :teacher_dedications
+
+  has_many :teacher_extension_activities
+  has_many :extension_activities, through: :teacher_extension_activities
+
   delegate :fullname, to: :profile
+
+  def total_credit_assignatures
+    self.teacher_assignatures.sum(:total_credit)
+  end
 end
