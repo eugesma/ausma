9d49bf5986858e8f4dd58cc0c11e90cff0ca8ef9
BasHud <eugesma@gmail.com>
2020-06-07 20:16:05 -0300

    Add observation to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_223206) do
+ActiveRecord::Schema.define(version: 2020_06_07_223207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
