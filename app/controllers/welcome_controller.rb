5a5cb07a698980377215b54477c995e759fab516
eugesma@gmail.com <eugesma@gmail.com>
2021-09-18 17:23:49 -0300

    fix: remove filter by year from teacher assignatures and extension activities

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,10 +5,10 @@ class WelcomeController < ApplicationController
     @teacher = current_user.teacher
     @year = params[:year].present? ? Date.new(params[:year].to_i) : Date.today
 
-    @assignatures = @teacher.teacher_assignatures_to_year(@year)
+    @assignatures = @teacher.teacher_assignatures
     @meetings = @teacher.teacher_meetings_to_year(@year)
     @formations = @teacher.teacher_formations_to_year(@year)
-    @extension_activities = @teacher.teacher_extension_activities_to_year(@year)
+    @extension_activities = @teacher.teacher_extension_activities
     @posts = @teacher.teacher_posts_to_year(@year)
     @projects = @teacher.teacher_projects_to_year(@year)
 
